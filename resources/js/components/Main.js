c369c49d48904d41e7764fad187c8948ebd7eda4
DESKTOP-4S2F2RT\Kok Chin <kokchin@diningbutler.com>
2018-09-14 14:23:31 +0800

    'ppp'

--- resources/js/components/Main.js
+++ resources/js/components/Main.js
@@ -6,7 +6,7 @@ class Main extends Component {
     render() {
         return (
             <div>
-                <h3>MasterPPPDDD</h3>
+                <h3>PLPL</h3>
             </div>
         );
     }
