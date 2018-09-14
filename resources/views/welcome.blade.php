53f3704008af7f35860641eb2d37463d109b1f4e
kckam <kamkokchin@gmail.com>
2018-09-14 15:01:45 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <link href="{{mix('/css/app.css')}}" rel="stylesheet" type="text/css">
     </head>
     <body>
-    <h2 style="text-align: center">vffvf</h2>
+    <h2 style="text-align: center">cdcd</h2>
         <div id="root"></div>
         <script src="{{mix('/js/app.js')}}" ></script>
     </body>
