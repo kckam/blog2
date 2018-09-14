8c2f47245527c559779af55dd700910f58d95a29
DESKTOP-4S2F2RT\Kok Chin <kokchin@diningbutler.com>
2018-09-14 12:39:11 +0800

    '213'

--- resources/js/components/Main.js
+++ resources/js/components/Main.js
@@ -6,7 +6,7 @@ class Main extends Component {
     render() {
         return (
             <div>
-                <h3>KIKIKI222</h3>
+                <h3>KIKIKI222lll</h3>
             </div>
         );
     }
