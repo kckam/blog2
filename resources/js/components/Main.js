fb765b33b0ec492b8dfaa77bbdce2c9d50bc299c
DESKTOP-4S2F2RT\Kok Chin <kokchin@diningbutler.com>
2018-09-14 14:02:56 +0800

    'ggg'

--- resources/js/components/Main.js
+++ resources/js/components/Main.js
@@ -6,7 +6,7 @@ class Main extends Component {
     render() {
         return (
             <div>
-                <h3>MasterPPP</h3>
+                <h3>MasterPPPDDD</h3>
             </div>
         );
     }
