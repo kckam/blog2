7daa04a37981554739d33e4d798aa657b1f978ea
DESKTOP-4S2F2RT\Kok Chin <kokchin@diningbutler.com>
2018-09-14 12:44:45 +0800

    '123'

--- resources/js/components/Main.js
+++ resources/js/components/Main.js
@@ -6,7 +6,7 @@ class Main extends Component {
     render() {
         return (
             <div>
-                <h3>KIKIKI222lll</h3>
+                <h3>jujujuj</h3>
             </div>
         );
     }
