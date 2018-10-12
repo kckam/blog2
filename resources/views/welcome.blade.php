dc0a3c92ddcb9c2f16fa1614919480c0c4de916b
Kam Kok Chin <kamkokchin@gmail.com>
2018-10-12 23:23:41 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <link href="{{mix('/css/app.css')}}" rel="stylesheet" type="text/css">
     </head>
     <body>
-    <h2 style="text-align: center">HAHAHAHA</h2>
+    <h2 style="text-align: center">Finally</h2>
     <h2 style="text-align: center">Development haha great</h2>
         <div id="root"></div>
         <script src="{{mix('/js/app.js')}}" ></script>
